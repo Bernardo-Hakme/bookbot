54da24f972dedd22c51b6abbec421a451daa5d05
Bernardo Hakme <bernardo.hakme04@gmail.com>
2025-08-05 22:54:47 +0200

    Getting Frankenstein's text

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def main():
+    frank_path = "/home/bernardo_hakme/workspace/GitHub/bookbot/books/frankenstein.txt"
+    frankenstein = get_book_text(frank_path)
+    print(frankenstein)
+    
+def get_book_text(filepath):
+    with open(filepath, "r") as f:
+        return f.read()
+    
+    
+
+
+
+
+main()
