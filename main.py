cf60a6bbf74b27eeef82076dc396f110b3c57f70
Bernardo Hakme <bernardo.hakme04@gmail.com>
2025-08-06 09:03:03 +0200

    Add word_count function, print it
    Changed the parameter name inside get_book_text

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 def main():
     frank_path = "/home/bernardo_hakme/workspace/GitHub/bookbot/books/frankenstein.txt"
     frankenstein = get_book_text(frank_path)
-    print(frankenstein)
-    
-def get_book_text(filepath):
-    with open(filepath, "r") as f:
+    total_words = word_count(frankenstein)
+    print(f"{total_words} words found in the document")
+
+#Gets file content
+def get_book_text(file_path):
+    with open(file_path, "r") as f:
         return f.read()
-    
+
+#Gets the number of words in a book
+def word_count(book_content):
+    num_words = book_content.split()
+    return len(num_words)
+
     
 
 
