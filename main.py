04f21e873221bf392b94c8ee46c2facc9495bdda
Bernardo Hakme <bernardo.hakme04@gmail.com>
2025-08-06 10:34:07 +0200

    Add Report and add char_stats in \n

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from stats import get_book_text, word_count, char_count, char_sort
 
+import sys
+
 def main():
     frank_path = "/home/bernardo_hakme/workspace/GitHub/bookbot/books/frankenstein.txt"
-    
+        
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {frank_path}")
+
+    print("------------ Word Count ------------")
     frankenstein = get_book_text(frank_path)
     total_words = word_count(frankenstein)
 
     print(f"{total_words} words found in the document")
-
+    
+    print("------------ Char Count ------------")
     total_chars = char_count(frankenstein)
     
     sorted_total_chars = char_sort(total_chars)
-    print(sorted_total_chars)
+    for i in sorted_total_chars:
+        if i["char"].isalpha():    
+            print(f"{i["char"]}: {i["num"]}")
+    print("============ END ============")
 
-    
     
 main()
