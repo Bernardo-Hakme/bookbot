777241514fcfa8c4e89dba4415ac01f54e113bed
Bernardo Hakme <bernardo.hakme04@gmail.com>
2025-08-22 12:40:03 +0200

    Finished report layout

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from stats import count_words, count_characters, sorted_characters
 
+import sys
+
 def get_book_text(filepath):
     with open(filepath, "r") as file:
         file_data = file.read()
     return file_data
 
 def main():
-    text_frankenstein = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_path = sys.argv[1]
+    text_book = get_book_text(file_path)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {file_path}")
 
     print("----------- Word Count ----------")
-    num_words = count_words(text_frankenstein)
+    num_words = count_words(text_book)
     print(f"Found {num_words} total words")
 
     print("--------- Character Count -------")
-    num_chars = count_characters(text_frankenstein)
+    num_chars = count_characters(text_book)
     pre_report = sorted_characters(num_chars)
     for d in pre_report:
         if d["char"].isalpha():
